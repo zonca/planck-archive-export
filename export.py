6c6c412a62d3953a13400775e9dc9770cfa4f875
Ben Rusholme <rusholme@caltech.edu>
2014-06-29 16:14:30 -0700

    Minor name changes to fit IRSA expected format; added attributes for metadata propagation; changed healpix nside from 2048 to 1024 for direct comparison with dpc without u_degrade

--- export.py
+++ export.py
@@ -49,38 +49,49 @@ for od in range(700, 1604+1):
     from dipole import Dipole
     dip = Dipole(obt=fits_file["OBT"].data["OBT"]/2**16, type="total") 
 
-    output_folder = "out/"
-    output_filename = "%d_%04d.h5" % (freq, od)
-
     madam_baselines = MadamBaselines(baselines_file % freq)
 
-    with h5py.File(output_folder + output_filename, 'w') as output_h5:
-        for ch in lfi.f[freq].ch[:1]:
+    for ch in lfi.f[freq].ch[:1]:
+
+        output_folder = "out/"
+        output_filename = "%03d-%s_%04d.hdf5" % (freq, ch, od)
+    
+        with h5py.File(output_folder + output_filename, 'w') as output_h5:
             print ch
             good_data = np.logical_or(~check_bit(fits_file["OBT"].data["FLAG"], 0), fits_file[ch.tag].data["FLAG"]==0) 
             n_good_data = good_data.sum()
-
+    
             data=np.zeros(n_good_data,dtype={
                     'names':fields,
                     'formats':['int64','int64','float64','float64','float64','int64','float64','float64','int64']
                                             })
-
+    
             # timing
             data["od"] = od
             obt = fits_file["OBT"].data["OBT"][good_data]/2**16
             data["utc"] = obt2utc(obt)
             data["ring"] = 0
-
+    
             # pointing
             theta, phi, psi = pnt.get_3ang(ch)
             data['glon'] =      np.degrees(phi[good_data])
             data['glat'] = 90 - np.degrees(theta[good_data])
             data['psi']  =      np.degrees(psi[good_data])
-            data['healpix_2048'] = hp.ang2pix(2048, theta[good_data], phi[good_data])
-
+            data['healpix_1024'] = hp.ang2pix(1024, theta[good_data], phi[good_data])
+    
             # dipole
             data['dipole'] = dip.get_4piconv_dx10(ch, theta, phi, psi)[good_data]
-
+    
             # tsky (LFI data are already dipoleremoved
             data['tsky'] = madam_baselines.baseline_remove(obt, fits_file[ch.tag].data[ch.tag][good_data], ch.tag)
-            output_h5.create_dataset(ch.tag, data=data, compression='lzf')
+            output_h5.create_dataset("%03d-%s" % (freq, ch), data=data, compression='lzf')
+    
+            # attributes
+            output_h5.attrs['PROCVER']  = 'DX11D'
+            output_h5.attrs['TELESCOP'] = 'PLANCK'
+            output_h5.attrs['INSTRUME'] = 'LFI'
+            output_h5.attrs['DETNAM']   = ch
+            output_h5.attrs['FREQ']     = str(freq).zfill(3)
+            output_h5.attrs['OBJECT']   = 'MISSION OPERATIONAL DAY '+str(od)
+            output_h5.attrs['FILENAME'] = output_filename
+            output_h5.attrs['TIMEZERO'] = '1958-01-01z00:00'
